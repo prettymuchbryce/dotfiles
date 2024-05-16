728ec4f4651d5ba117dccebf5a9f361a5ba515b0
Bryce Neal <brycedneal@gmail.com>
2024-05-16 07:44:27 -0400

    Copilot no markdown files

--- modules/nvim/nvim/lua/plugins/copilot.lua
+++ modules/nvim/nvim/lua/plugins/copilot.lua
@@ -4,5 +4,7 @@ return {
     vim.g.copilot_assume_mapped = true
     vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap('i', '<C-o>', 'copilot#Accept("<C-o>")', { silent = true, expr = true })
+    -- Don't spy on my markdown files
+    vim.g.copilot_filetypes = { markdown = false }
   end,
 }
