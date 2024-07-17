20cfdf5219f966b89db095eefbe2ba82c3806bb0
Bryce Neal <brycedneal@gmail.com>
2024-07-17 09:51:49 -0400

    Bufferline, tmux, hosts

--- modules/nvim/nvim/lua/plugins/bufferline.lua
+++ modules/nvim/nvim/lua/plugins/bufferline.lua
@@ -1,14 +1,9 @@
 return {
-  'bufferlineplugin',
-  name = 'bufferline',
-  dev = { true },
-  version = '*',
+  'akinsho/bufferline.nvim',
   dependencies = 'nvim-tree/nvim-web-devicons',
   config = function()
     require('bufferline').setup()
 
-    -- TODO: Fix this.
-    -- TODO: Install lualine.
     vim.api.nvim_set_keymap('n', '<C-k>', ':BufferLineCycleNext<CR>', { silent = true })
     vim.api.nvim_set_keymap('n', '<C-l>', ':BufferLineCyclePrev<CR>', { silent = true })
   end,
